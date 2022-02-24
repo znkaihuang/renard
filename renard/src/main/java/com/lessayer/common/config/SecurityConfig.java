b2b75b5bddcfb813734bc6261b7fc6129c9540ad
znkaihuang <zkhuang@protonmail.com>
2022-02-24 15:18:46 +0800

    renardClient
    Change-Id: I71e4f52ca12c35cf586db98973abdea4122d8395
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renard/src/main/java/com/lessayer/common/config/SecurityConfig.java
+++ renard/src/main/java/com/lessayer/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.lessayer.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -41,4 +42,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.httpBasic();
 		
 	}
+	
 }
--- renard/src/main/java/com/lessayer/common/controller/ViewController.java
+++ renard/src/main/java/com/lessayer/common/controller/ViewController.java
@@ -1,12 +1,15 @@
 package com.lessayer.common.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ViewController {
 	
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String displayHomePage() {
 		
 		return "index";
