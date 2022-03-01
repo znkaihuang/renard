f311453d01a91d926268abba2f9bb86648bf3037
znkaihuang <zkhuang@protonmail.com>
2022-03-01 21:55:45 +0800

    Refactor application entry point
    Change-Id: I09c53ce318a98f8b63c22e00bde139aaa4b0716b
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renardServer/src/main/java/com/lessayer/common/RenardServerApplication.java
+++ renardServer/src/main/java/com/lessayer/common/RenardServerApplication.java
@@ -1,13 +1,33 @@
 package com.lessayer.common;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RenardServerApplication {
-
+public class RenardServerApplication implements CommandLineRunner {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(RenardServerApplication.class, args);
+	
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		logger.info("Starting renard service...");
+		
+	}
+	
 }
