562dad763237148c02417dacd647da44b9e58f99
znkaihuang <zkhuang@protonmail.com>
2022-03-02 19:43:53 +0800

    Add Json annotation to Company class
    Change-Id: If9668627815f5faf73ef9c17b325bd0a6a6db58e
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renardServer/src/main/java/com/lessayer/common/entity/Company.java
+++ renardServer/src/main/java/com/lessayer/common/entity/Company.java
@@ -1,47 +1,83 @@
 package com.lessayer.common.entity;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Company {
 	
-	private String requestInfoDate; // 出表日期
-	private String companyId; // 公司代號
-	private String companyName; // 公司名稱
-	private String companyAbbrev; // 公司簡稱
-	private String foreignEntrepreneurRegisMap; // 外國企業註冊地國
-	private String industryType; // 產業別
-	private String address; // 住址
-	private String unifiedBusinessNum; // 營利事業統一編號
-	private String chairman; // 董事長
-	private String generalManager; // 總經理
-	private String spokesman; // 發言人
-	private String spokesmanTitle; // 發言人職稱
-	private String deputySpokesman; // 代理發言人
-	private String telephone; // 總機電話
-	private String incorporationDate; // 成立日期
-	private String listingDate; // 上市日期
-	private String commonStockParValue; // 普通股每股面額
-	private String capital; // 實收資本額
-	private String privateStockShares; // 私募股數
-	private String preferredStockShares; // 特別股
-	private String financialReportType; // 編制財務報表類型
-	private String stockTransferAgent; // 股票過戶機構
-	private String stockTransferAgentTelephone; // 過戶電話
-	private String stockTransferAgentAddress; // 過戶地址
-	private String accountingFirm; // 簽證會計師事務所
-	private String accountant1; // 簽證會計師1
-	private String accountant2;   // 簽證會計師2
-	private String symbol; // 英文簡稱
-	private String englishAddress; // 英文通訊地址
-	private String fax; // 傳真機號碼
-	private String email; // 電子郵件信箱
-	private String website; // 網址
+	@JsonAlias("出表日期")
+	private String requestInfoDate;
+	@JsonAlias("公司代號")
+	private String companyId;
+	@JsonAlias("公司名稱")
+	private String companyName; 
+	@JsonAlias("公司簡稱")
+	private String companyAbbrev; 
+	@JsonAlias("外國企業註冊地國")
+	private String foreignEntrepreneurRegisMap; 
+	@JsonAlias("產業別")
+	private String industryType;
+	@JsonAlias("住址")
+	private String address;
+	@JsonAlias("營利事業統一編號")
+	private String unifiedBusinessNum;
+	@JsonAlias("董事長")
+	private String chairman;
+	@JsonAlias("總經理")
+	private String generalManager;
+	@JsonAlias("發言人")
+	private String spokesman;
+	@JsonAlias("發言人職稱")
+	private String spokesmanTitle;
+	@JsonAlias("代理發言人")
+	private String deputySpokesman;
+	@JsonAlias("總機電話")
+	private String telephone;
+	@JsonAlias("成立日期")
+	private String incorporationDate;
+	@JsonAlias("上市日期")
+	private String listingDate; 
+	@JsonAlias("普通股每股面額")
+	private String commonStockParValue;
+	@JsonAlias("實收資本額")
+	private String capital;
+	@JsonAlias("私募股數")
+	private String privateStockShares;
+	@JsonAlias("特別股")
+	private String preferredStockShares;
+	@JsonAlias("編制財務報表類型")
+	private String financialReportType;
+	@JsonAlias("股票過戶機構")
+	private String stockTransferAgent;
+	@JsonAlias("過戶電話")
+	private String stockTransferAgentTelephone;
+	@JsonAlias("過戶地址")
+	private String stockTransferAgentAddress;
+	@JsonAlias("簽證會計師事務所")
+	private String accountingFirm;
+	@JsonAlias("簽證會計師1")
+	private String accountant1;
+	@JsonAlias("簽證會計師2")
+	private String accountant2;
+	@JsonAlias("英文簡稱")
+	private String symbol;
+	@JsonAlias("英文通訊地址")
+	private String englishAddress;
+	@JsonAlias("傳真機號碼")
+	private String fax;
+	@JsonAlias("電子郵件信箱")
+	private String email;
+	@JsonAlias("網址")
+	private String website;
 	
 }
