1ca7e8249984014d8faedae190c480d39e6fcd9b
znkaihuang <zkhuang@protonmail.com>
2022-03-09 14:44:57 +0800

    Comment SecurityConfig for now
    Change-Id: Ia5e1d3c9b4ca8ce26d45c749dd088916e6a036cd
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renardClient/src/main/java/com/lessayer/common/config/SecurityConfig.java
+++ renardClient/src/main/java/com/lessayer/common/config/SecurityConfig.java
@@ -1,46 +1,42 @@
 package com.lessayer.common.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	private final PasswordEncoder passwordEncoder = 
-			PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	
-	@Bean
-	UserDetailsService authentication() {
-		
-		UserDetails user = User.builder()
-									.username("kevin")
-									.password(passwordEncoder.encode("123"))
-									.roles("ADMIN")
-									.build();
-		return new InMemoryUserDetailsManager(user);
-		
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.authorizeRequests()
-			.mvcMatchers("/").hasRole("ADMIN")
-			.anyRequest().authenticated()
-			.and()
-			.formLogin()
-			.and()
-			.httpBasic();
-		
-	}
-	
-}
+/*
+ * import org.springframework.context.annotation.Bean; import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import
+ * org.springframework.security.config.annotation.web.builders.WebSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * EnableWebSecurity; import
+ * org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter; import
+ * org.springframework.security.core.userdetails.User; import
+ * org.springframework.security.core.userdetails.UserDetails; import
+ * org.springframework.security.core.userdetails.UserDetailsService; import
+ * org.springframework.security.crypto.factory.PasswordEncoderFactories; import
+ * org.springframework.security.crypto.password.PasswordEncoder; import
+ * org.springframework.security.provisioning.InMemoryUserDetailsManager;
+ */
+/*
+ * @EnableWebSecurity public class SecurityConfig extends
+ * WebSecurityConfigurerAdapter {
+ * 
+ * private final PasswordEncoder passwordEncoder =
+ * PasswordEncoderFactories.createDelegatingPasswordEncoder();
+ * 
+ * @Bean UserDetailsService authentication() {
+ * 
+ * UserDetails user = User.builder() .username("kevin")
+ * .password(passwordEncoder.encode("123")) .roles("ADMIN") .build(); return new
+ * InMemoryUserDetailsManager(user);
+ * 
+ * }
+ * 
+ * @Override protected void configure(HttpSecurity http) throws Exception {
+ * 
+ * http.authorizeRequests() .mvcMatchers("/").hasRole("ADMIN")
+ * .anyRequest().authenticated() .and() .formLogin() .and() .httpBasic();
+ * 
+ * }
+ * 
+ * }
+ */
