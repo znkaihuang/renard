c1dceba9b290d4b1e6cc78db35588f97e60ae8e3
znkaihuang <zkhuang@protonmail.com>
2022-03-11 20:48:34 +0800

    Add companStockHistory page
    Change-Id: Ic50f9c3f79318a87a3b9aefccf1c46753f0ded37
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renardClient/src/main/java/com/lessayer/common/controller/ViewController.java
+++ renardClient/src/main/java/com/lessayer/common/controller/ViewController.java
@@ -1,10 +1,7 @@
 package com.lessayer.common.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ViewController {
@@ -13,6 +10,13 @@ public class ViewController {
 	public String displayHomePage() {
 		
 		return "index";
+		
+	}
+	
+	@GetMapping("/companyStockHistory")
+	public String displayCompanStockHistory() {
+		
+		return "companyStockHistory";
 		
 	}
 	
