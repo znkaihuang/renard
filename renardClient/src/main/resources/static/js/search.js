53a2262d1df2d28832c0ba48a65e60810057dff4
znkaihuang <zkhuang@protonmail.com>
2022-03-14 15:26:38 +0800

    Plot company history index
    Change-Id: I90088b32ff1e05f8dd1a1125f979aa68667a0e03
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renardClient/src/main/resources/static/js/search.js
+++ renardClient/src/main/resources/static/js/search.js
@@ -2,6 +2,28 @@
  * 
  */
 
+let currentDateTime = new Date();
+let currentYear = currentDateTime.getFullYear().toString();
+let currentMonth = (currentDateTime.getMonth() >= 9) ? (currentDateTime.getMonth() + 1).toString()
+												: "0" + (currentDateTime.getMonth() + 1).toString();
+let currentDate = (currentDateTime.getDate() >= 10) ? currentDateTime.getDate().toString()
+												: "0" + currentDateTime.getDate().toString();
+
+class Stock {
+	constructor(date, tradeVolume, tradeValue, openingPrice,
+		highestPrice, lowestPrice, closingPrice, change, transaction) {
+		this.date = date;
+		this.tradeVolume = tradeVolume;
+		this.tradeValue = tradeValue;
+		this.openingPrice = openingPrice;
+		this.highestPrice = highestPrice;
+		this.lowestPrice = lowestPrice;
+		this.closingPrice = closingPrice;
+		this.change = change;
+		this.transaction = transaction;
+	}
+}
+
 var companyList;
 (async function init() {
 	companyList = await getAllCompaniesInfo();
@@ -53,7 +75,6 @@ function showInstantInfo(companyId) {
 	    	return response.json();
 	  	})
 	  	.then(function(instantIndexInfo) {
-			console.log(instantIndexInfo);
 	    	document.getElementById("instantIndex").innerHTML = instantIndexInfo[0].currentPrice;
 	    	document.getElementById("todayIndexHighest").innerHTML = instantIndexInfo[0].highestPrice;
 	    	document.getElementById("todayIndexLowest").innerHTML = instantIndexInfo[0].lowestPrice;
@@ -63,6 +84,119 @@ function showInstantInfo(companyId) {
 	    });
 }
 
-function showCompanyHistory(companyId) {
+function showCompanyHistory() {
+	var companyId = document.getElementById("companyInstantIndexSearch").value.substr(0, 4);
+	getIndexHistoryInfo(companyId);
 	return false;
+}
+
+let indexHistoryArray = [];
+function getIndexHistoryInfo(companyId) {
+	
+	fetch("http://localhost:8081/renardServer/historyInfo_" 
+		+ currentYear + currentMonth + "01_" + companyId)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(async function(indexHistory) {
+			//console.log(indexHistory.length);
+			console.log(indexHistory);
+			for(var i = 0; i < indexHistory.length; i++) {
+				indexHistoryArray.push(
+					new Stock(transformDate(indexHistory[i].date), indexHistory[i].tradeVolume,
+						indexHistory[i].tradeValue, indexHistory[i].openingPrice,
+						indexHistory[i].highestPrice, indexHistory[i].lowestPrice,
+						indexHistory[i].closingPrice, indexHistory[i].change,
+						indexHistory[i].transaction));
+			}
+			plotChart(indexHistoryArray);
+			createIndexTable(indexHistoryArray);
+		});
+}
+
+function transformDate(date) {
+	var dateSplit = date.split("/");
+	dateSplit[0] = (parseInt(dateSplit[0]) + 1911).toString();
+	return dateSplit.join("-");
+}
+
+function createIndexTable(data) {
+	let indexTable = document.createElement("table");
+	let tableHeader = document.createElement("thead");
+	let tableBody = document.createElement("tbody");
+	tableHeader.innerHTML = "<tr><th>日期</th><th>當日開盤</th><th>當日最高</th>"+
+							"<th>當日最低</th><th>當日收盤</th></tr>"
+	let bodyString = "";
+	for(var i = data.length - 1; i >= 0; i--) {
+		bodyString += createIndexRow(data[i]);
+	}
+	tableBody.innerHTML = bodyString;
+	indexTable.append(tableHeader);
+	indexTable.append(tableBody);
+	document.getElementById("indexTable").append(indexTable);
+}
+
+function createIndexRow(rowData) {
+	return "<tr><td>" + rowData.date + "</td><td>" + rowData.openingPrice + "</td><td>"
+		+ rowData.highestPrice + "</td><td>" + rowData.lowestPrice + "</td><td>"
+		+ rowData.closingPrice + "</td></tr>";
+}
+
+function plotChart(dataObjectArray) {
+	var arrayLength = dataObjectArray.length;
+	
+	var dateArray = [];
+	var closingPriceArray = [];
+	var highestPriceArray = [];
+	var lowestPriceArray = [];
+	var openingPriceArray = [];
+	for(var i = 0; i < arrayLength; i++) {
+		dateArray.push(dataObjectArray[i].date);
+		closingPriceArray.push(stringToFloat(dataObjectArray[i].closingPrice));
+		highestPriceArray.push(stringToFloat(dataObjectArray[i].highestPrice));
+		lowestPriceArray.push(stringToFloat(dataObjectArray[i].lowestPrice));
+		openingPriceArray.push(stringToFloat(dataObjectArray[i].openingPrice));
+	}
+	
+	var trace1 = {
+  		x: dateArray,
+  		close: closingPriceArray,
+  		decreasing: {line: {color: 'green'}}, 
+		high: highestPriceArray,
+		increasing: {line: {color: 'red'}}, 
+		low: lowestPriceArray,
+		open: openingPriceArray,
+		type: 'ohlc', 
+		xaxis: 'x', 
+		yaxis: 'y',
+		line: {
+			width: 5
+		}
+	};
+	
+	var data = [trace1];
+	var layout = {
+		dragmode: 'zoom', 
+		margin: {
+			r: 10, 
+			t: 25, 
+			b: 40, 
+			l: 60
+		}, 
+		showlegend: false, 
+		xaxis: {
+    		autorange: true, 
+			title: '日期', 
+			type: 'date'
+		}, 
+		yaxis: {
+			autorange: true, 
+			type: 'linear'
+		}
+	};
+	Plotly.newPlot("plotChart", data, layout);
+}
+
+function stringToFloat(numberString) {
+	return parseFloat(numberString.split(",").join(""));
 }
