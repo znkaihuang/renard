929c25aca66329cba1c854bb109f7e0ef04f1498
znkaihuang <zkhuang@protonmail.com>
2022-03-14 15:43:44 +0800

    Gather duplicate js function to common.js
    Change-Id: I1e54e65c848e759915554597af2087c1a255c6b2
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renardClient/src/main/resources/static/js/script.js
+++ renardClient/src/main/resources/static/js/script.js
@@ -1,46 +1,14 @@
 /**
- * Global Field
- */
-
-let currentDateTime = new Date();
-let currentYear = currentDateTime.getFullYear().toString();
-let currentMonth = (currentDateTime.getMonth() >= 9) ? (currentDateTime.getMonth() + 1).toString()
-												: "0" + (currentDateTime.getMonth() + 1).toString();
-let currentDate = (currentDateTime.getDate() >= 10) ? currentDateTime.getDate().toString()
-												: "0" + currentDateTime.getDate().toString();
-class Stock {
-	constructor(date, tradeVolume, tradeValue, openingPrice,
-		highestPrice, lowestPrice, closingPrice, change, transaction) {
-		this.date = date;
-		this.tradeVolume = tradeVolume;
-		this.tradeValue = tradeValue;
-		this.openingPrice = openingPrice;
-		this.highestPrice = highestPrice;
-		this.lowestPrice = lowestPrice;
-		this.closingPrice = closingPrice;
-		this.change = change;
-		this.transaction = transaction;
-	}
-}
-
-/**
  * Daily Info
  */
 
 let doOnceFlag = false;
 getInstantInfo(); 
 setInterval(getInstantInfo, 30000);
-(function getDateTime() {
-	let dateTimeWorker;
-	if(typeof(dateTimeWorker) == "undefined") {
-		dateTimeWorker = new Worker("js/time_worker.js");
-	}
-	dateTimeWorker.onmessage = function(event) {
-		document.getElementById("currentDate").innerHTML = event.data[0];
-		document.getElementById("currentTime").innerHTML = event.data[1];
-	};
-})();
 
+(function init() {
+	getDateTime();
+}) ();
 
 function getInstantInfo() {
 	if(isCurrentTimeAvailForInstantInfo()) {
