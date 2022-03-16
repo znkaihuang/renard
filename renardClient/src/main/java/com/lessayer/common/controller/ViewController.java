06e66a5f891d89f8df6562021bbb8e1e37f5940b
znkaihuang <zkhuang@protonmail.com>
2022-03-16 15:19:04 +0800

    Add controller method for post request
    Change-Id: I3c8393a03e876ada2ecba519bb6c40f3b8a2a74c
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- renardClient/src/main/java/com/lessayer/common/controller/ViewController.java
+++ renardClient/src/main/java/com/lessayer/common/controller/ViewController.java
@@ -1,10 +1,16 @@
 package com.lessayer.common.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/home")
 	public String displayHomePage() {
@@ -31,6 +37,16 @@ public class ViewController {
 	public String displayOrderSystem() {
 		
 		return "orderSystem";
+		
+	}
+	
+	@PostMapping("/orderSystem")
+	public void dealOrder(@RequestParam String stockId, @RequestParam String price,
+			@RequestParam String volume, String buyButton, String sellButton) {
+		
+		logger.info("Post request to /orderSystem");
+		logger.info("stockid {}, price {}, volume {}, buyButton {}, sellButton {}",
+				stockId, price, volume, buyButton, sellButton);
 		
 	}
 	
